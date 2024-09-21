9b0e8da896adbee85aa00b1dfabe397bb94f8d22
kenteus henry <protoolsent@Mac-mini.attlocal.net>
2024-09-21 12:24:09 -0400

    KensBranch

--- KensPage.ts
+++ KensPage.ts
@@ -2,18 +2,20 @@ import { By } from "selenium-webdriver";
 import { BasePage } from './basepage';
 
 export class savorShop extends BasePage {
-    cart: By = By.xpath('//path[@d="M27.5762 7.91198C27.4058 7.69053 27.1418 7.55425 26.8607 7.55425H21.1796L17.0827 0.527362C16.8357 0.10149 16.2821 -0.0433063 15.8562 0.203699C15.4303 0.450705 15.2855 1.00434 15.5325 1.43021L19.1013 7.54573H9.16149L12.7303 1.43021C12.9773 1.00434 12.8325 0.450705 12.4066 0.203699C11.9808 -0.0433063 11.4271 0.0929728 11.1801 0.527362L7.08324 7.55425H1.39359C1.11251 7.55425 0.848471 7.68201 0.678122 7.91198C0.507773 8.13344 0.456669 8.43155 0.524808 8.69559L4.11917 21.4292C4.22989 21.821 4.57911 22.085 4.98795 22.085H23.2579C23.6582 22.085 24.0159 21.821 24.1266 21.4292L27.721 8.69559C27.7976 8.42303 27.7465 8.13344 27.5762 7.91198ZM22.585 20.2878H5.66934L2.58603 9.35143H6.03559L4.90277 11.2934C4.65577 11.7193 4.80056 12.2729 5.22644 12.5199C5.37123 12.6051 5.52455 12.6392 5.67786 12.6392C5.98449 12.6392 6.2826 12.4773 6.45295 12.1963L8.11385 9.34291H20.149L21.8099 12.1963C21.9802 12.4858 22.2784 12.6392 22.585 12.6392C22.7383 12.6392 22.8916 12.5966 23.0364 12.5199C23.4623 12.2729 23.6071 11.7193 23.3601 11.2934L22.2272 9.35143H25.6768L22.585 20.2878Z"]');
-    search: By = By.xpath('//input[@class="form-field-input live-search-form-field form-field-mobile-custom snize-input-style"][2]');
+    cart: By = By.xpath('//span[text()="Cart"]')
+    search: By = By.xpath('(//input[@name="q"])[1]')
     freeShip: By = By.css('announcement-bar-link');
     coupon: By = By.id('email_51606746');
     storeLocator: By = By.css('.home-store');
-    checkout: By = By.name('[name="checkout"]')
+    checkout: By = By.name('checkout')
     searchBtn: By = By.xpath('(//button[@class="live-search-button custom-live-search-button mdc-ripple-surface mdc-ripple-upgraded"])[1]');
     signUp: By = By.xpath('//button[@class="needsclick go1608708250 kl-private-reset-css-Xuajs1"]');
-    locationSearch: By = By.xpath('.mapboxgl-ctrl-geocoder--input');
+    locationSearch: By = By.xpath('//input[@class="mapboxgl-ctrl-geocoder--input"]');
     findStoreBtn: By = By.xpath('//button[text()="Find stores near my current location"]');
     popUp: By = By.xpath('//button[@class = "needsclick klaviyo-close-form go260142404 kl-private-reset-css-Xuajs1"]');
-
+    seasoning: By = By.xpath('(//img[@class="productitem--image-primary"])[1]');
+    addToCart: By = By.xpath ('//span[@class = "atc-button--text"]');
+    pumpkinPie: By = By.xpath('(//span[@class="snize-thumbnail"])[1]'); 
     constructor(){
         super({url:'https://www.savoryspiceshop.com/'});
     };
--- kensTestPage.test.ts
+++ kensTestPage.test.ts
@@ -1,38 +1,44 @@
 import { savorShop } from "./KensPage";
 const spice = new savorShop();
+const fs = require('fs')
 
 test ('Checking cart for product', async () => {
     await spice.navigate();
-    await spice.click(spice.cart);
+    await spice.click(spice.popUp); 
+    await spice.click(spice.seasoning); 
+    await spice.click(spice.addToCart);
     await spice.click(spice.checkout);
     await spice.driver.navigate().back();
+    await spice.click(spice.cart); 
+   
 
 });
 
 test ('Search for item', async () => {
     await spice.navigate();
-    await spice.click(spice.search);
-    await spice.click(spice.searchBtn)
+    await spice.setInput(spice.search, "Pumpkin Pie Spice\n");
+    await spice.click(spice.pumpkinPie); 
     await spice.driver.navigate().back();
-
+    
 });
 
 test ('10% coupon', async () => {
     await spice.navigate();
-    await spice.scrollToElement(spice.coupon)
-    await spice.click(spice.coupon);
-    await spice.click(spice.signUp)
+    await spice.scrollToElement(spice.coupon);
+    await spice.setInput(spice.coupon, "spiceOfLife@gmail.com");
+    await fs.writeFile(`${__dirname}/coupon.png`, 
+        await spice.driver.takeScreenshot(), "base64", (e) => {
+                if(e) console.error(e)
+                else console.log('page saved') 
+    });
     await spice.driver.navigate().back();
-
 });
 
 test ('Store Locator feature', async () => {
     await spice.navigate();
     await spice.click(spice.storeLocator);
-    await spice.click(spice.locationSearch);
+    await spice.setInput(spice.locationSearch, "Atlanta\n");
     await spice.click(spice.findStoreBtn)
     await spice.driver.navigate().back();
     await spice.driver.quit();
-
 });
-
